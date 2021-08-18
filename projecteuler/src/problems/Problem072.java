f1862d9234410d7e5c852146157145d3306adfd2
Bheta <chinmaygovind314@gmail.com>
2021-08-18 19:47:23 -0400

    Solved Problem 73
    it loops.

--- projecteuler/src/problems/Problem072.java
+++ projecteuler/src/problems/Problem072.java
@@ -1,28 +1,42 @@
 package problems;
 
 
+import util.Numbers;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+
 //Ordered Fractions.
 public class Problem072 {
-    private static final double limit = 3/7.0;
-    private static double maxFraction = 0;
-    private static int maxFractionNumerator = 0;
-    private static int maxFractionDenominator = 0;
+    private static ArrayList<Integer> primes = Numbers.generatePrimes(1000);
+    private static long totalFractions = 0;
     public static void main(String[] args) {
-        for (double i = 1; i < 3.1 / 7.0 * 1000000; i++){
-            for (int offset = -2; offset < 3; offset++) {
-                int denominator = (int) Math.round(7.0 / 3 * i) + offset;
-                double fraction = i / denominator;
-                if (fraction < limit && fraction > maxFraction && denominator <= 1000000) {
-                    maxFraction = fraction;
-                    maxFractionNumerator = (int) i;
-                    maxFractionDenominator = denominator;
-                }
-            }
+        for (int i = 2; i <= 1000000; i++){
+            totalFractions += totient(i);
         }
-
-        System.out.println("The numerator of the proper fraction in the ordered list of proper fractions with the denominator below 1,000,000 is: " + maxFractionNumerator);
-        System.out.println("The fraction is: " + maxFractionNumerator + "/" + maxFractionDenominator);
-        System.out.println("The value of the fraction is: " + maxFraction);
+        System.out.println("The number of proper fractions with the denominator up to one million is: " + totalFractions);
     }
 
+
+    private static int totient(int num){
+        HashSet<Integer> factors = new HashSet<>(getPrimeFactors(num));
+        for (int factor : factors){
+            num *= (1 - (1.0 / factor));
+        }
+        return Math.round(num);
+    }
+    private static ArrayList<Integer> getPrimeFactors(int num){
+        ArrayList<Integer> factors = new ArrayList<>();
+        for (Integer prime : primes) {
+            if (num == 1) break;
+            while (num % prime == 0) {
+                factors.add(prime);
+                num /= prime;
+            }
+        }
+        if (num != 1) {
+            factors.add(num);
+        }
+        return factors;
+    }
 }
---
+++ projecteuler/src/problems/Problem074.java
@@ -0,0 +1,43 @@
+package problems;
+
+
+import util.Numbers;
+
+import java.util.ArrayList;
+
+//Ordered Fractions.
+public class Problem074 {
+    private static ArrayList<Integer> primes = Numbers.generatePrimes(350);
+    private static long totalFractions = 0;
+    public static void main(String[] args) {
+        for (int i = 2; i <= 5999; i++){
+            ArrayList<Integer> factors = getPrimeFactors(i);
+            for (int j = 2 * i + 1; j <= Math.min(3 * i, 12001) - 1; j++){
+                boolean relativelyPrime = true;
+                for (int factor : factors){
+                    if (j%factor == 0){
+                        relativelyPrime = false;
+                        break;
+                    }
+                }
+                if (relativelyPrime) totalFractions++;
+            }
+        }
+        System.out.println("The number of proper fractions between 1/2 and 1/3 with the denominator up to 12,000 is: " + totalFractions);
+    }
+
+    private static ArrayList<Integer> getPrimeFactors(int num){
+        ArrayList<Integer> factors = new ArrayList<>();
+        for (Integer prime : primes) {
+            if (num == 1) break;
+            while (num % prime == 0) {
+                factors.add(prime);
+                num /= prime;
+            }
+        }
+        if (num != 1) {
+            factors.add(num);
+        }
+        return factors;
+    }
+}
