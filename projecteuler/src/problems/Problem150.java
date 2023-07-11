6144db37b00b500bf60fb63da3245616e60600f3
NinjaMango <65680585+NinjaMango@uQsers.noreply.github.com>
2023-07-11 15:00:52 -0400

    PROBLEM ONE FIFTYYYYY
    it was trivial???  what????
    idk  why i never just brute forced it i think i accidentally transposed the triangle the first time lol

--- projecteuler/src/problems/Problem150.java
+++ projecteuler/src/problems/Problem150.java
@@ -3,12 +3,9 @@ package problems;
 
 import util.Timer;
 
-import java.util.Arrays;
-
 //Searching a Triangular Array for a Sub-Triangle Having Minimum-Sum.
 public class Problem150 {
     private static final long[] s = new long[500500];
-    private static int answer = 0;
 
     public static void main(String[] args) {
         Timer.startTimer();
@@ -20,34 +17,14 @@ public class Problem150 {
         }
         int[][] grid = new int[1000][1000];
         int total = 0;
-        Node[] nodes = new Node[500500];
         for (int row = 0; row < grid.length; row++) {
             for (int i = 0; i <= row; i++) {
-                grid[i][row] = (int) s[total];
+                grid[row][i] = (int) s[total];
                 total++;
             }
         }
-        for (int row = grid.length - 1; row >= 0; row--) {
-            for (int i = row; i >= 0; i--) {
-                total--;
-                nodes[total] = new Node(grid[row][i], null, null);
-                if (row < grid.length - 1) {
-                    //left is grid entry one row down
-                    nodes[total].left = nodes[total + (row + 1)];
-                    nodes[total].right = nodes[total + (row + 1) + 1];
-                }
-            }
-        }
-        System.out.println(nodes[0]);
-        System.out.println(nodes[1]);
-
-
-
-        //System.out.println(grid[0][0]);
-        //System.out.println(grid[0][1]);
-        //System.out.println(grid[1][1]);
         /*
-        int[][] grid = new int[][]{
+        grid = new int[][]{
                 {15, 0, 0, 0, 0, 0},
                 {-14, -7, 0, 0, 0, 0},
                 {20, -13, -5, 0, 0, 0},
@@ -55,27 +32,28 @@ public class Problem150 {
                 {1, -4, -5, -18, 5, 0},
                 {-16, 31, 2, 9, 28, 3}
         };
+        */
 
-         */
-        /*
-        Timer.startTimerNano();
+        Timer.startTimer();
+        long absoluteMin = Long.MAX_VALUE;
         for (int startRow = 0; startRow < grid.length; startRow++) {
+            //System.out.println("startRow: " + startRow);
             //System.out.println(startRow);
-            for (int startCol = 0; startCol < startRow; startCol++) {
-                for (int extend = 1; extend < grid.length - startRow - startCol + 1; extend++) {
-                    int working = 0;
-                    for (int i = 0; i < extend; i++) {
-                        for (int j = 0; j <= i; j++) {
-                            working += grid[startRow + i][startCol + j];
-                        }
+            for (int startCol = 0; startCol <= startRow; startCol++) {
+                int extend = grid.length - startRow;
+                long working = 0;
+                for (int i = 0; i < extend; i++) {
+                    for (int j = 0; j <= i; j++) {
+                        working += grid[startRow + i][startCol + j];
                     }
-                    //System.out.println("working: " + working);
-                    min = Math.min(min, working);
+                    absoluteMin = Math.min(absoluteMin, working);//check after each row of triangle is added
                 }
+                //System.out.println("working: " + working);
+                
             }
         }
-
-         */
+        System.out.println("The smallest possible triangle sum is: " + absoluteMin);
+        System.out.println("Ran in " + Timer.elapsedTime() + "ms");
 
         /*
         Timer.startTimer();
@@ -110,7 +88,6 @@ public class Problem150 {
          */
         //System.out.println(Arrays.deepToString(newGrid).replace("],", "]\n"));
         //System.out.println("The smallest possible triangle sum is: " + absoluteMin);
-        System.out.println("Ran in " + Timer.elapsedTime() + "ms");
     }
 
 
