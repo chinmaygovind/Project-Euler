617abd604dda8096e563bffb3f11bbd8cb38b556
NinjaMango <65680585+NinjaMango@uQsers.noreply.github.com>
2023-07-27 16:05:19 -0400

    solved problem 153
    how to solve any euler problem:
    1. come up with the stupid naive way
    2. make it slightly less stupid
    3. repeat step 2 until it runs within a minute
    4. profit

--- projecteuler/src/problems/Problem153.java
+++ projecteuler/src/problems/Problem153.java
@@ -7,42 +7,92 @@ import util.Numbers;
 //Investigating Gaussian Integers.
 public class Problem153 {
     
-    private static final int LIMIT = 5;
+    private static final int LIMIT = 100_000_000;
     private static final double SQRT_LIMIT = Math.sqrt(LIMIT);
-    private static ArrayList<Integer> primes = Numbers.generatePrimes(2 * LIMIT);//generate excess so gaussian primes can be collected
+    private static ArrayList<Integer> primes = Numbers.generatePrimes(2 * LIMIT);
     private static long total = 0;
     public static void main(String[] args) {
-
+        /*
         //find all gaussian primes up to SQRT_LIMIT
         long total = 0;
         ArrayList<GaussianInteger> gaussianPrimes = new ArrayList<>();
-        //a + bi is a gaussian prime iff:
-        //1. a or b is zero and absolute value of the other is a prime of form 4n + 3
-        for (int p : primes) {
-            if (p%4 == 3) {
-                gaussianPrimes.add(new GaussianInteger(p));
-            }
-        }
-        for (int a = 1; a <= LIMIT/2; a++) {
-            for (int b = 1; b <= LIMIT/2; b++) {
+        for (int a = 0; a <= SQRT_LIMIT; a++) {
+            for (int b = 0; b <= SQRT_LIMIT; b++) {
+                //a + bi is a gaussian prime iff:
+                //1. a or b is zero and absolute value of the other is a prime of form 4n + 3
+                //actually we're gonna ignore this case for now
                 //2. both a and b are non-zero and a^2 + b^2 is prime
                 if (primes.contains(a*a + b*b)) {
                     gaussianPrimes.add(new GaussianInteger(a, b));
-                    gaussianPrimes.add(new GaussianInteger(a, -b)); //ignore the conjugate
+                    //gaussianPrimes.add(new GaussianInteger(a, -b)); //ignore the conjugate
                 }
             }
         }
         System.out.println(gaussianPrimes);
         //add up all the primes' factors, including multiples of the primes
-        ArrayList<GaussianInteger> allTheShits = new ArrayList<>();
-        for (int p1 = 0; p1 < gaussianPrimes.size(); p1++) {
-            for (int p2 = 0; p2 <gaussianPrimes.size(); p2++) {
-
+        for (int pIndex = 0; pIndex < gaussianPrimes.size(); pIndex++) {
+            GaussianInteger p = gaussianPrimes.get(pIndex);
+            int norm = p.norm();
+            int repetitions = LIMIT / norm;
+            int counts = 0;
+            //count all the integer multiples of the prime
+            for (long f = 1; f <= repetitions; f++) {
+                counts += (repetitions / f) * f;//this is a cursed line of code but i promise it works
             }
+            total += counts * (2 * p.getReal());
+            //count all the :JDSL:FJS:LDJFklsdjflksdjflsies
+            for (int p2Index = pIndex + 1; p2Index < gaussianPrimes.size(); p2Index++) {
+                GaussianInteger product = p.multiply(gaussianPrimes.get(p2Index));
+                int norm2 = product.norm();
+                int repetitions2 = LIMIT / norm2;
+                int counts2 = 0;
+                for (long f = 1; f <= repetitions2; f++) {
+                counts += (repetitions2 / f) * f;//this is a cursed line of code but i promise it works
+                }
+                total += counts2 * (2 * product.getReal());
+            }
+
+            System.out.println("Counting " + p + " " + (repetitions + (repetitions) * (repetitions - 1) / 2) + " times");
+
+        }
+        System.out.println("fast method complex only: " + total);
+        for (long f = 1; f <= LIMIT; f++) {
+            total += (LIMIT / f) * f;//this is a cursed line of code but i promise it works
         }
         System.out.println("fast method: " + total);
-        System.out.println("slow method: " + slowSumDivisors(LIMIT));
+        */
         
+        for (long a = 1; a <= SQRT_LIMIT; a++) {
+            //if (a%10 == 0) System.out.printf("a: %d\n", a);
+            for (long b = 1; b <= a; b++) {
+                if (Numbers.GCF(a, b) != 1) continue;
+                long norm = a * a + b * b;//we know that if a + bi is a factor, a - bi is also a factor.
+                long contribution = 0;
+                int k = 1;
+                //so, (a + bi)(a - bi) = a^2 + b^2 is a factor. 
+                //so, all multiples of a * a + b * b have a + bi and a - bi as a factor.
+                //so, we can add (a + bi) + (a - bi) = 2a to the sum
+                while (k * norm <= LIMIT) {
+                    long temp = LIMIT / (norm * k);
+                    if (b < a) {
+                        temp *= k * ((2 * a) + (2 * b));
+                    } else {
+                        temp *= k * (2 * a);
+                    }
+                    contribution += temp;
+                    k++;
+                    
+                }
+                
+                total += contribution;
+            }
+        }
+        //System.out.println(contributions);
+        //count all rational integer divisors
+        for (long f = 1; f <= LIMIT; f++) {
+            total += (LIMIT / f) * f;
+        }
+        System.out.println("The sum of Gaussian Integer divisors up to 10^8 is: " + total);
         
 
     }
@@ -53,8 +103,9 @@ public class Problem153 {
         //count all GaussianInteger     long total = 0;
         //stupid naive logic is correct
         long total = 0;
+        //cut in half by only
         for (long a = 1; a <= limit/2; a++) {
-            if (a%1000 == 0) System.out.printf("a: %d\n", a);
+            //if (a%1000 == 0) System.out.printf("a: %d\n", a);
             for (long  b = 1; b <= limit/2; b++) {
                 long norm = a * a + b * b;//we know that if a + bi is a factor, a - bi is also a factor.
                 long adjustedNorm = norm / Numbers.GCF(a, b);
