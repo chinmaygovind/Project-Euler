2fbc5c20ba297ce8a586924acc554c3d32b6b41b
Bheta <65680585+TheBheta@users.noreply.github.com>
2024-03-01 14:12:14 -0500

    Update Problem165.java

--- projecteuler/src/problems/Problem165.java
+++ projecteuler/src/problems/Problem165.java
@@ -1,7 +1,13 @@
 package problems;
 
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.HashSet;
+
 //Intersections.
 public class Problem165 {
+
+    private static HashSet<Double> foundPoints = new HashSet<>();
 
     public static void main(String[] args) {
         long[] s = new long[20001];
@@ -12,38 +18,42 @@ public class Problem165 {
             t[i] = (int) (s[i]%500);
         }
         int intersections = 0;
-        System.out.println(intersects(27, 44, 12, 32, 46, 53, 17, 62));
-        System.out.println(intersects(46, 70, 22, 40, 46, 53, 17, 62));
-        System.out.println(intersects(27, 44, 12, 32, 46, 70, 22, 40));
-        for (int l1 = 0; l1 < t.length/8; l1++) {
-            int x1 = t[8*l1+1];
-            int y1 = t[8*l1+2];
-            int x2 = t[8*l1+3];
-            int y2 = t[8*l1+4];
+        for (int l1 = 0; l1 < t.length/4; l1++) {
+            int x1 = t[4*l1+1];
+            int y1 = t[4*l1+2];
+            int x2 = t[4*l1+3];
+            int y2 = t[4*l1+4];
             //(y2 - y1)/(x2 - x1)
             //y = m(x - x1) + y1
-            for (int l2 = l1+1; l2 < t.length/8; l2++) {
-                int x3 = t[8*l1+5];
-                int y3 = t[8*l1+6];
-                int x4 = t[8*l1+7];
-                int y4 = t[8*l1+8];
-                intersections += intersects(x1, y1, x2, y2, x3, y3, x4, y4) ? 1 : 0;
+            for (int l2 = l1; l2 < t.length/4; l2++) {
+                int x3 = t[4*l2+1];
+                int y3 = t[4*l2+2];
+                int x4 = t[4*l2+3];
+                int y4 = t[4*l2+4];
+                intersects(x1, y1, x2, y2, x3, y3, x4, y4);
 
             }
         }
-        System.out.println(intersections);
+        System.out.println(foundPoints.size());
     }
 
-    public static boolean intersects(int x1, int y1, int x2, int y2, int x3, int y3, int x4, int y4) {
-        double m1 = ((double) (y2 - y1)) / (x2 - x1);
-        double m2 = ((double) (y4 - y3)) / (x4 - x3);
+    private static void intersects(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4) {
+        double m1 = (y2 - y1) / (x2 - x1);
+        double m2 = (y4 - y3) / (x4 - x3);
+        //System.out.println(m1 + ", " + m2);
         if (m1 == m2) {
-            return false;
+            return;
         }
         //m1(x - x1) + y1 = m2(x - x3) + y3
         //x(m1 - m2) = x1 * m1 -x3 * m2 + y3 - y1
+        //System.out.printf("Line A: y = %.2f(x - %.2f) + %.2f\n", m1, x1, y1);
+        //System.out.printf("Line B: y = %.2f(x - %.2f) + %.2f\n", m2, x3, y3);
+        //System.out.printf("(%.2f - %.2f)x = %.2f * %.2f - %.2f * %.2f + %.2f - %.2f\n", m1, m2, x1, m1, x3, m2, y3, y1);
         double x = (x1 * m1 - x3 * m2 + y3 - y1) / (m1 - m2);
-        return x > Math.max(Math.min(x1, x2), Math.min(x3, x4)) && x < Math.min(Math.max(x1, x2), Math.max(x3, x4));
+        //System.out.println(x);
+        if (x > Math.max(Math.min(x1, x2), Math.min(x3, x4)) && x < Math.min(Math.max(x1, x2), Math.max(x3, x4))) {
+            foundPoints.add(x * (m1 * (x - x1) + y1));//xy
+        }
     }
 }
 
